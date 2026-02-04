e3d211915d63eab2585603fd7347b42546ce793f
Just Ordinary <68330655+normalycreate@users.noreply.github.com>
2026-02-04 18:18:03 +0700

    First Calculator Structure With Basic Features

---
+++ JS Function/script.js
@@ -0,0 +1,6 @@
+console.log(`Selamat datang di website kami`)
+let ekspresi = "";
+function masukanAngka(number) {
+    ekspresi += number;
+    document.getElementById("tampilan").value = ekspresi;
+}
